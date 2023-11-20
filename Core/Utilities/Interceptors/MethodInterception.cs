21484768a464f12917413624c5fc8fd39a2c4fd3
kadiiravsaar <kadiiravsar@gmail.com>
2023-11-20 18:32:58 +0300

    Interceptors added

---
+++ Core/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,40 @@
+﻿using Castle.DynamicProxy;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, System.Exception e) { }
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+
+        
+
+
+        public override void Intercept(IInvocation invocation)  // Invocation => Metotum
+        {
+            var isSuccess = true;
+            OnBefore(invocation); // metottan önce çalıştır (metodun başında)
+            try
+            {
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e); // Hata aldığında
+                throw; 
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation); // başarılı olduğunda 
+                }
+            }
+            OnAfter(invocation); // metottan sonra çalışsın
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+namespace Core.Utilities.Interceptors // Interceptors   yol kesmek demek
+{
+
+    // *************************** classlara | metotlara ekleyebilirsin, birden fazla ekleyebilirsin, inherit edilen noktada da çalışsın
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; } // hangi attribute önce çalışsın. Sıralama için
+
+        public virtual void Intercept(IInvocation invocation) //  Invocation => metot demek bunu unutma
+        {                                                                             
+            // ne yapacağını boş bıraktım ilerde doludurcam
+        }
+    }
+}
